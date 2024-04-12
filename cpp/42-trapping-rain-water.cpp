41706ef9acfd73c82b52c2ecd17a48795e10068f
Morărescu Mihnea <mihneathm@gmail.com>
2024-04-12 09:30:45 +0300

    added 42

---
+++ cpp/42-trapping-rain-water.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+class Solution {
+public:
+    int trap(std::vector<int>& height) {
+        int n = height.size();
+        if (n == 0) {
+            return 0;
+        }
+        
+        std::vector<int> left(n);
+        std::vector<int> right(n);
+
+        left[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = std::max(left[i - 1], height[i]);
+        }
+
+        right[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = std::max(right[i + 1], height[i]);
+        }
+
+        int trapped = 0;
+        for (int i = 0; i < n; i++) {
+            trapped += std::min(left[i], right[i]) - height[i];
+        }
+
+        return trapped;
+    }
+};
