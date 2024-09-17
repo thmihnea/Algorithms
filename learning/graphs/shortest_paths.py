20de3a0e160ed1d23a4393a5a2b6434972d56e0b
thmihnea <mihneathm@gmail.com>
2024-09-17 15:33:21 +0300

    add shortest path api

--- learning/graphs/shortest_paths.py
+++ learning/graphs/shortest_paths.py
@@ -1,3 +1,6 @@
+from queue import LifoQueue
+from typing import List
+
 class DirectedEdge(object):
 
     def __init__(self, v: int, w: int, weight: float):
@@ -5,13 +8,13 @@ class DirectedEdge(object):
         self.w = w
         self.weight = weight
 
-    def _from(self):
+    def _from(self) -> int:
         return self.v
     
-    def _to(self):
+    def _to(self) -> int:
         return self.w
     
-    def weight(self):
+    def weight(self) -> float:
         return self.weight
     
 class EdgeWeightedDigraph(object):
@@ -24,11 +27,30 @@ class EdgeWeightedDigraph(object):
         v: int = edge._from()
         self.adj[v].append(edge)
 
-    def edges(self, v: int):
+    def edges(self, v: int) -> List[DirectedEdge]:
         return self.adj[v]
+    
+    def size(self) -> int:
+        return self.v
     
 class ShortestPath(object):
 
     def __init__(self, graph: EdgeWeightedDigraph, source: int):
-        self.graph = graph
-        self.source = source
+        self.graph: EdgeWeightedDigraph = graph
+        self.source: int = source
+        self.dist_to: List[float | int] = [0 if i == source else float("inf") for i in range(graph.size())]
+        self.edge_to: List[DirectedEdge] = [None for _ in range(graph.size())]
+
+    def distance(self, v: int) -> float:
+        return self.dist_to[v]
+    
+    def get_path(self, v: int) -> LifoQueue:
+        # Returns a stack containing all the edges on
+        # the path. Continuously popping the stack
+        # will yield the correct path.
+        stack = LifoQueue()
+        edge: DirectedEdge = self.edge_to[v]
+        while edge is not None:
+            stack.put(edge)
+            edge = self.edge_to[edge._from()]
+        return stack
