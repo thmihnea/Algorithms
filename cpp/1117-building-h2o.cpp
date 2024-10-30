3e480af73a1f66d18e6d3a6433b661996df2df18
thmihnea <mihneathm@gmail.com>
2024-10-30 15:22:15 +0000

    added 1117

---
+++ cpp/1117-building-h2o.cpp
@@ -0,0 +1,29 @@
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+
+std::mutex gMutex;
+std::condition_variable gCdVar;
+
+class H2O {
+private:
+    int m_hydrogenCount;
+public:
+    H2O() : m_hydrogenCount(0) {}
+
+    void hydrogen(std::function<void()> releaseHydrogen) {
+        std::unique_lock<std::mutex> lock(gMutex);
+        gCdVar.wait(lock, [&]{ return this->m_hydrogenCount < 2; });
+        releaseHydrogen();
+        this->m_hydrogenCount += 1;
+        gCdVar.notify_all();
+    }
+
+    void oxygen(std::function<void()> releaseOxygen) {
+        std::unique_lock<std::mutex> lock(gMutex);
+        gCdVar.wait(lock, [&]{ return this->m_hydrogenCount == 2; });
+        releaseOxygen();
+        this->m_hydrogenCount = 0;
+        gCdVar.notify_all();
+    }
+};
